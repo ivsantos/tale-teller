5f49951bf6b260ece236a13846f162f3f9afa405
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-01-29 19:13:48 +0100

    feat: Go down to tale functionality

--- src/app/components/Tale/Tale.tsx
+++ src/app/components/Tale/Tale.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { cohereResponse, generateResponse } from 'cohere-ai/dist/models';
+import { useRef, useState } from 'react';
 
-import { useState } from 'react';
+import DownImage from '@/images/down.png';
+import Image from 'next/image';
 
 interface Tale extends cohereResponse<generateResponse> {}
 
 export default function Tale() {
   const [input, setInput] = useState<string>('Once upon a time');
   const [tale, setTale] = useState<Tale>();
+  const ref = useRef<HTMLElement>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
@@ -20,9 +23,13 @@ export default function Tale() {
     setTale(tale);
   };
 
+  const handleScroll = () => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <>
-      <div className="w-80 grid grid-flow-col gap-4 mx-auto my-0">
+      <div className="w-80 grid grid-flow-col gap-4 mx-auto mb-10">
         <input
           className="p-2 border-2 border-gray-300 rounded-md"
           placeholder="Once upon a time..."
@@ -37,10 +44,24 @@ export default function Tale() {
         </button>
       </div>
       {tale && (
-        <article className="mx-8 px-2 mt-8 text-center whitespace-pre-line lg:mx-[unset] border-x-2 border-x-gray-500 border-dashed italic">
-          <h2>Title of the tale</h2>
-          <p>{tale.body.generations[0].text}</p>
-        </article>
+        <section className="place-items-center grid gap-6">
+          <button onClick={handleScroll}>
+            <Image
+              className="animate-bounce"
+              alt="Go down"
+              src={DownImage}
+              width={64}
+              height={64}
+            />
+          </button>
+          <article
+            ref={ref}
+            className="mx-8 px-2 mt-8 text-center whitespace-pre-line lg:mx-[unset] border-x-2 border-x-gray-500 border-dashed italic"
+          >
+            <h2>Title of the tale</h2>
+            <p>{tale.body.generations[0].text}</p>
+          </article>
+        </section>
       )}
     </>
   );
