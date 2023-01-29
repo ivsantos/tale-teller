a6790683abcac3f0e7f99140917a82aaf7dff42f
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-01-29 19:14:16 +0100

    feat: Add subheader and external link to cohere following a11y

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import ExternalIcon from '@/images/external-link.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import LogoImage from '@/images/logo.png';
@@ -5,7 +6,7 @@ import MoonImage from '@/images/moon.png';
 
 export default function Header() {
   return (
-    <header className="grid h-48 gap-4 mx-4 text-center">
+    <header className="grid h-48 mx-4 text-center">
       <Image
         className="animate-wiggle left-4 absolute -mt-2"
         alt="Moon"
@@ -15,8 +16,8 @@ export default function Header() {
         priority
       />
       <nav>
-        <ul className="flex items-center justify-center h-full">
-          <li className="-mt-8">
+        <ul className="flex items-center justify-center h-full mb-5">
+          <li>
             <Link href="/" title="Back to homepage">
               <h1>
                 <Image
@@ -31,6 +32,26 @@ export default function Header() {
           </li>
         </ul>
       </nav>
+      <h2 className="italic">
+        <a
+          title="External link to Cohere AI"
+          className="underline"
+          href="https://dashboard.cohere.ai/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AI-based
+          <Image
+            className="inline-block mx-1 mb-1"
+            alt="Cohere AI Logo"
+            src={ExternalIcon}
+            width={14}
+            height={14}
+            priority
+          />
+        </a>{' '}
+        tale generator conceived for a better sleep 💤
+      </h2>
     </header>
   );
 }
