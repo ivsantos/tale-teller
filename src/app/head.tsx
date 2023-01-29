7905519b625149973d29d5ccc2d04b63872771e2
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-01-29 18:35:48 +0100

    feat: Change favicon to logo one

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,9 +5,9 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="A web app that tells AI-generated tales based on your input."
+        content="A web app that generates and tells AI-based tales given an input."
       />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" />
     </>
   );
 }
