dca72345498358cbb378a2de658a9d0b8720fb6b
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-01-29 18:25:05 +0100

    feat: Add title to header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,23 +1,32 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import LogoImage from '@/images/logo-no-background.png';
 import MoonImage from '@/images/moon.png';
 
 export default function Header() {
   return (
-    <header className="grid gap-4 mx-4 mb-12 text-center">
+    <header className="grid h-48 gap-4 mx-4 text-center">
+      <Image
+        className="animate-wiggle left-4 absolute -mt-2"
+        alt="Moon"
+        src={MoonImage}
+        width={150}
+        height={150}
+        priority
+      />
       <nav>
-        <ul>
-          <li className="flex items-center">
-            <Image
-              alt="Moon"
-              className="animate-wiggle -mt-2"
-              src={MoonImage}
-              priority
-              width={150}
-              height={150}
-            />
+        <ul className="flex items-center justify-center h-full">
+          <li className="-mt-8">
             <Link href="/" title="Back to homepage">
-              <h1 className="text-2xl">Tale Teller</h1>
+              <h1>
+                <Image
+                  alt="Tale Teller Logo"
+                  src={LogoImage}
+                  width={250}
+                  height={250}
+                  priority
+                />
+              </h1>
             </Link>
           </li>
         </ul>
