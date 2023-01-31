711fdcdafd280812382da7d6a924a616367893f4
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-01-31 22:43:15 +0100

    style: :lipstick: Add styles for generate button

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -35,16 +35,18 @@ export default function Form({
           onChange={onInputChange}
         />
         <button
-          className="flex items-center justify-center gap-2 p-4 bg-white rounded-lg"
           type="submit"
+          className="gap-2 relative inline-flex items-center justify-center p-0.5 font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
         >
-          {loading ? <span>Loading...</span> : <span>Generate!</span>}
-          <Image
-            className={`${loading ? 'animate-wandwave' : ''}`}
-            alt="An icon of a magical wand"
-            src={WandIcon}
-            width={32}
-          />
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full flex justify-between">
+            {loading ? <span>Loading...</span> : <span>Generate!</span>}
+            <Image
+              className={`${loading ? 'animate-wandwave' : ''}`}
+              alt="An icon of a magical wand"
+              src={WandIcon}
+              width={32}
+            />
+          </span>
         </button>
       </div>
     </form>
