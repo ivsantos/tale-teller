b22351cac3fa556e079b3424df4b53515d8ba034
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-01-31 19:02:01 +0100

    feat: :wheelchair: Improve input a11y

--- src/app/components/Tale/Tale.tsx
+++ src/app/components/Tale/Tale.tsx
@@ -70,29 +70,34 @@ export default function Tale() {
     <>
       <form
         onSubmit={handleGenerate}
-        className="w-80 grid grid-flow-col gap-4 mx-auto mb-10"
+        className="lg:max-w-md max-w-sm mx-auto mb-10"
       >
-        <input
-          className={`w-full p-2 border-2 rounded-md ${
-            error ? 'error' : 'border-gray-300'
-          }`}
-          placeholder="Once upon a time..."
-          type="text"
-          value={input}
-          onChange={handleInputChange}
-        />
-        <button
-          className="flex items-center justify-center p-4 bg-white rounded-lg"
-          type="submit"
-        >
-          <span>Generate!</span>
-          <Image
-            className="inline"
-            alt="An icon of a magical wand"
-            src={WandIcon}
-            width={32}
+        <label htmlFor="ai-input">Please, tell me a story!</label>
+        <div className="grid-cols-form grid grid-flow-col gap-4">
+          <input
+            className={`w-full p-2 border-2 rounded-md ${
+              error ? 'error' : 'border-gray-300'
+            }`}
+            type="text"
+            id="ai-input"
+            name="ai-input"
+            placeholder="Once upon a time..."
+            value={input}
+            onChange={handleInputChange}
           />
-        </button>
+          <button
+            className="flex items-center justify-center p-4 bg-white rounded-lg"
+            type="submit"
+          >
+            <span>Generate!</span>
+            <Image
+              className="inline"
+              alt="An icon of a magical wand"
+              src={WandIcon}
+              width={32}
+            />
+          </button>
+        </div>
       </form>
       {tale && (
         <div className="place-content-center grid mb-4">
