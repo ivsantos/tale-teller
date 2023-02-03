3e657979d6e40fab98a9d9989ad1a10b1ef1ac67
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-02-03 22:08:41 +0100

    refactor: :recycle: Adapt use cover hook to midjourney client

--- src/app/hooks/useCover/useCover.tsx
+++ src/app/hooks/useCover/useCover.tsx
@@ -4,43 +4,13 @@ interface UseCoverPredictionProps {
   input: string;
 }
 
-interface IPrediction {
-  completed_at: Date;
-  created_at: Date;
-  error: string | null;
-  id: string;
-  input: Input;
-  logs: string;
-  metrics: Metrics;
-  output: string[];
-  started_at: Date;
-  status: 'starting' | 'processing' | 'succeeded' | 'failed' | 'canceled';
-  urls: Urls;
-  version: string;
-  webhook_completed: string | null;
-}
-
-interface Input {
-  prompt: string;
-}
-
-interface Metrics {
-  predict_time: number;
-}
-
-interface Urls {
-  get: string;
-  cancel: string;
-}
-
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
-
 export default function useCoverPrediction({ input }: UseCoverPredictionProps) {
-  const [prediction, setPrediction] = useState<IPrediction | null>(null);
+  const [prediction, setPrediction] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   const handleGetPrediction = useCallback(async () => {
+    setLoading(true);
     const prompt = `mdjrny-v4 style a highly detailed matte painting of ${input} by studio ghibli, makoto shinkai, by artgerm, by wlop, by greg rutkowski, volumetric lighting, octane render, 4 k resolution, trending on artstation, masterpiece`;
     const config = {
       method: 'POST',
@@ -49,40 +19,20 @@ export default function useCoverPrediction({ input }: UseCoverPredictionProps) {
       },
       body: JSON.stringify({ prompt }),
     };
-    const response = await fetch(`/api/cover?prompt=${prompt}`, config);
-    let predicted = await response.json();
-    setPrediction(predicted);
-
-    while (
-      predicted?.status !== 'succeeded' &&
-      predicted?.status !== 'failed'
-    ) {
-      await sleep(2000);
-      const response = await fetch(`/api/cover/${predicted.id}`);
-      predicted = await response.json();
-      if (response.status !== 200) {
-        setLoading(false);
-        return setError(predicted.detail);
-      }
-      setPrediction(predicted);
+    const response = await fetch(`/api/cover`, config);
+    if (response.status !== 200) {
+      setLoading(false);
+      const error = await response.json();
+      return setError(error.detail);
     }
+    let predicted = (await response.json()) as string[];
+    setLoading(false);
+    setPrediction(predicted.at(-1));
   }, [input]);
 
   useEffect(() => {
     handleGetPrediction();
   }, [handleGetPrediction]);
-
-  useEffect(() => {
-    if (
-      prediction?.status === 'starting' ||
-      prediction?.status === 'processing'
-    ) {
-      setLoading(true);
-    }
-    if (prediction?.status === 'succeeded') {
-      setLoading(false);
-    }
-  }, [prediction]);
 
   return { prediction, loading, error };
 }
