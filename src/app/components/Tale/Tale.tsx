c7fb5c969e88e928acc569258fc7b6d7c3ac6476
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-02-03 23:29:22 +0100

    style: :lipstick: Change spacing for tale section

--- src/app/components/Tale/Tale.tsx
+++ src/app/components/Tale/Tale.tsx
@@ -46,7 +46,7 @@ export default function Tale({ children, tale, input }: TaleProps) {
       {children}
       {tale && (
         <section ref={ref} className="place-items-center grid gap-6 mt-16">
-          <article className="mx-8 px-2 mt-8 text-center whitespace-pre-line lg:mx-[unset] border-x-2 border-x-gray-500 border-dashed italic">
+          <article className="mx-8 p-2 mb-8 text-center whitespace-pre-line lg:mx-[unset] border-2 border-gray-500 border-dashed italic">
             <button
               className="active:scale-95 shadow-gray-400 w-36 flex flex-col items-center gap-2 p-2 mx-auto my-0 border-2 border-gray-500 rounded-md shadow-md"
               type="button"
