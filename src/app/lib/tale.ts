466236ca31623b1c2766d72f6dd0d4edb1948243
Ivan Santos Panadero <ivan.santos.sge@mango.com>
2023-02-03 22:49:32 +0100

    refactor: :recycle: Expose API key to browser

--- src/app/lib/tale.ts
+++ src/app/lib/tale.ts
@@ -5,7 +5,7 @@ export default async function generate(input: string) {
     return null;
   }
 
-  cohere.init(process.env.COHERE_API_KEY!, '2022-12-06');
+  cohere.init(process.env.NEXT_PUBLIC_COHERE_API_KEY!, '2022-12-06');
   const response = await cohere.generate({
     model: 'command-xlarge-20221108',
     prompt: `Give me a happy tale that starts with: "${input}" with a definite ending.`,
